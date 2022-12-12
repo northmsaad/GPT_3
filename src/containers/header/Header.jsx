6ae64196785309177454125c7412143370e39215
northmsaad <msddr010@gmail.com>
2022-12-12 14:07:45 +0500

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAi
+        </h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, at.
+          Laudantium porro delectus perferendis hic quos molestiae ipsum unde
+          eius. Fugit, aut nobis placeat numquam quaerat, perspiciatis ipsa,
+          repudiandae
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your E-mail Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="People" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="AI Image" />
+      </div>
     </div>
   );
 };
